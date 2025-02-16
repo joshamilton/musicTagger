56033e4531169da41896f7b4fff4e8a69d124496
Joshua J. Hamilton <joshamilton@gmail.com>
2025-02-16 12:40:39 -0800

    Modify cleanup script to also report whether albums have a log and cue

--- utils/cleanup.py
+++ utils/cleanup.py
@@ -3,6 +3,8 @@
 ### Copyright (c) 2025, Joshua J Hamilton
 ### This utility program finds and removes files that are not flac, cue, log or
 ### pdf. In addition, it renames files with uppercase extensions to lowercase.
+### Finally, the script reports all albums that are missing either a flac or
+### cue file.
 ### The script can be run in dry-run mode to generate a report without making
 ### any changes to the filesystem.
 ################################################################################
@@ -10,6 +12,7 @@
 ################################################################################
 ### Import packages
 ################################################################################
+import csv
 import os
 import argparse
 from tqdm import tqdm
@@ -85,6 +88,22 @@ def generate_report(files_to_rename, files_to_delete, busy_files_rename, busy_fi
         for file in busy_files_delete:
             busy_file.write(f"{file}\n")
 
+def generate_missing_files_report(directory):
+    print('Generating missing files report...')
+    report_data = []
+    for root, _, files in os.walk(directory):
+        has_flac = any(file.lower().endswith('.flac') for file in files)
+        if has_flac:
+            has_log = any(file.lower().endswith('.log') for file in files)
+            has_cue = any(file.lower().endswith('.cue') for file in files)
+            if not has_log or not has_cue:
+                report_data.append([root, 'Yes' if has_log else 'No', 'Yes' if has_cue else 'No'])
+
+    with open("missing.csv", "w", newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['Folder', 'Log', 'Cue'])
+        csvwriter.writerows(report_data)
+
 ################################################################################
 ### Define main function
 ################################################################################
@@ -110,5 +129,8 @@ def main():
         generate_report(files_to_rename, files_to_delete, busy_files_rename, busy_files_delete)
         print(f"Operation complete. {len(files_to_rename)} files renamed, {len(files_to_delete)} files deleted, and {len(busy_files_rename) + len(busy_files_delete)} files could not be processed due to being busy.")
 
+    generate_missing_files_report(args.dir)
+    print("Missing files report generated.")
+    
 if __name__ == "__main__":
     main()
